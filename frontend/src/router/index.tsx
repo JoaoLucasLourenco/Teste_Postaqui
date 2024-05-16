5203cdd28e8d33e03d8d4c148b7fc1699a8cbd58
João Lucas Lourenço <jao.lucas.lourenco@gmail.com>
2024-05-16 17:07:28 -0300

    feat: Routes
    Primeira rota e rota global declarada

---
+++ frontend/src/router/index.tsx
@@ -0,0 +1,13 @@
+
+import React from 'react';
+import {Routes, Route, Navigate} from 'react-router-dom'
+
+export const  AppRoutes = () =>{
+    return(
+        <Routes>
+            <Route path='/home' element={<p>HOME</p>}/>
+
+            <Route path='*' element={<Navigate to='/home'/>}/>
+        </Routes>
+    )
+};

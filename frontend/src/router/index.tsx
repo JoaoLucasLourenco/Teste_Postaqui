86e7582ce68b669a643c0cee2df5a097662c5efd
João Lucas Lourenço <jao.lucas.lourenco@gmail.com>
2024-05-16 18:15:28 -0300

    Config: install and test mui
    Instalado e testado Material UI

--- frontend/src/router/index.tsx
+++ frontend/src/router/index.tsx
@@ -1,11 +1,12 @@
 
+import { Button, ButtonBase } from '@mui/material';
 import React from 'react';
 import {Routes, Route, Navigate} from 'react-router-dom'
 
 export const  AppRoutes = () =>{
     return(
         <Routes>
-            <Route path='/home' element={<p>HOME</p>}/>
+            <Route path='/home' element={<Button variant='contained'>Jao</Button>}/>
 
             <Route path='*' element={<Navigate to='/home'/>}/>
         </Routes>
